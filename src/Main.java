adae2b2945af2fb60a81365400c92ea616fc9e74
Mathys Sclafer <mathys.sclafer@edu.esiee-it.fr>
2024-10-07 15:10:24 +0200

    Update Hello zizi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Zizi");
         System.out.println("Hhzfjzedipi");
         System.out.println("master");
         System.out.println("master");
