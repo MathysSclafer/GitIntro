9f4b91af9331f0b644506d2c0ebb3ea046ca2970
Mathys Sclafer <mathys.sclafer@edu.esiee-it.fr>
2024-10-07 13:51:15 +0200

    Modif master

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Hhzfjzedipi");
+        System.out.println("master");
+        System.out.println("master");
     }
 }
